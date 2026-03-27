106c6df1f3907a0883bd337172b3bfb992c82e78
Gimanthi Warsha <gimanthiwarsha6@gmail.com>
2026-03-27 23:43:20 +0530

    Update appRouter.jsx

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,17 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "../pages/Home";
 import SocietyRequestSubmit from "../pages/SocietyRequestSubmit";
 import SocietyRequestsAdmin from "../pages/SocietyRequestsAdmin";
 import SocietyRequestDetail from "../pages/SocietyRequestDetail";
 import SocietyRegisterByToken from "../pages/SocietyRegisterByToken";
-import AdminDashboard from "../pages/AdminDashboard";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SocietyRequestSubmit />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/submit" element={<SocietyRequestSubmit />} />
         <Route path="/admin/requests" element={<SocietyRequestsAdmin />} />
-        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin" element={<Navigate to="/admin/requests" replace />} />
         <Route path="/admin/requests/:id" element={<SocietyRequestDetail />} />
         <Route path="/society-register/:token" element={<SocietyRegisterByToken />} />
       </Routes>
