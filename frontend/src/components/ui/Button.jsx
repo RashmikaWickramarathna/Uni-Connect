3df47b9c9dd6525045218d4cf80321c28d7005ad
Rashmika Wickramarathna <it23794184@my.sliit.lk>
2026-04-03 22:29:15 +0530

    Add files via upload

---
+++ frontend/src/components/ui/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button as StyledButton } from './Button.styled';
+
+export function Button({ children, variant = 'default', size = 'default', className = '', fullWidth = false, ...props }) {
+  return (
+    <StyledButton 
+      $variant={variant} 
+      $size={size} 
+      $fullWidth={fullWidth}
+      className={className}
+      {...props}
+    >
+      {children}
+    </StyledButton>
+  );
+}
