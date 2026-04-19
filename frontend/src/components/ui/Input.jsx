a035a8374d813e14ef2aedf30230ab0d222e21bd
Rashmika Wickramarathna <it23794184@my.sliit.lk>
2026-04-19 14:28:37 +0530

    Delete frontend directory

--- frontend/src/components/ui/Input.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { InputStyled } from './Input.styled';
-
-export function Input({ className = '', ...props }) {
-  return <InputStyled className={className} {...props} />;
-}
